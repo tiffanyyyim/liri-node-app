47be9fbf2e1c180ff30be6778552a527b7339355
tiffanyyyim <yim.tiffany@gmail.com>
2017-10-31 22:41:54 -0400

    Add package.json

---
+++ keys.js
@@ -0,0 +1,12 @@
+```JavaScript
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'SbZhhKMo4RBgcCsfJFefgytHt',
+  consumer_secret: 'ni3dQZtt47SykUsrTiIaQjPth6D0f35mtbNW4uLP3pdbzDzGG6',
+  access_token_key: '922591021195825152-3XMKsxJx3omsH6HUIaW3DA2MRATxB3K',
+  access_token_secret: 'Ngir3EiiJjbxrLIp41k9cKJZH0Z8qVAnPjqj8vsVPHtdY',
+}
+
+module.exports = twitterKeys;
+```
